70138d772d1f86bcd3fe293464a5f03456eff660
Wikan Wicaksono <wikan.wicaksono@weekendinc.com>
2018-03-07 15:48:51 +0700

    new updates setrip mobile 80%

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,29 @@ if ('addEventListener' in document) {
       FastClick.attach(document.body);
   }, false);
 }
+
+// login, register & profile
+$('.home-datetime__option').click(function(){
+	var content = $(this).parents('.home-datetime__choose');
+	content.find('.input-icon').addClass('active');
+})
+$('.home-datetime__option').blur(function(){
+	var content = $(this).parents('.home-datetime__choose');
+	content.find('.input-icon').removeClass('active');
+})
+$('.open--login-head').on('click', function(){
+	$.login();
+})
+$('.header__login').on('click', function(){
+	$('.header__profile--menu').toggleClass('active')
+})
+
+// mobile menu
+function openMenuMobile(){
+	$('.sidemenu').addClass('active');
+	$('body').css('overflow', 'hidden');
+}
+function closeMenuMobile(){
+	$('.sidemenu').removeClass('active');
+	$('body').css('overflow', 'auto');
+}
